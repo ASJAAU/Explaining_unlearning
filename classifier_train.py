5f5811bc39e6ba12a341aa1260db57f35891342b
Anders <asjo@create.aau.dk>
2024-06-06 14:11:35 +0200

    Readability + logging

--- classifier_train.py
+++ classifier_train.py
@@ -1,15 +1,18 @@
 import argparse
 from datetime import datetime
 import tqdm
+import yaml
 
 from models.resnet import *
 from data.dataloader import REPAIHarborfrontDataset
 from utils.metrics import Logger, get_metrics
 from utils.saving import existsfolder, get_config
+
 import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2 as torch_transforms
 import timm
+
 
 if __name__ == "__main__":
     #CLI
@@ -35,7 +38,11 @@ if __name__ == "__main__":
         torch_transforms.ToDtype(torch.float32, scale=True),
         torch_transforms.ToTensor(),
     ])
+    label_transforms  = torch_transforms.Compose([
+        torch_transforms.ToTensor(),
+    ])
 
+    #Create datasets and dataloaders
     print("\n########## LOADING DATA ##########")
     train_dataset = REPAIHarborfrontDataset(
         data_split=cfg["data"]["train"],
@@ -43,6 +50,7 @@ if __name__ == "__main__":
         classes=cfg["data"]["classes"],
         transform=train_transforms,
         target_format=cfg["data"]["target_format"],
+        device=args.device,
         verbose=True, #Print status and overview
         )
 
@@ -52,6 +60,7 @@ if __name__ == "__main__":
         classes=cfg["data"]["classes"],
         transform=valid_transforms,
         target_format=cfg["data"]["target_format"],
+        device=args.device,
         verbose=True, #Print status and overview
         )
     
@@ -67,7 +76,7 @@ if __name__ == "__main__":
         valid_dataset,
         batch_size=cfg["training"]["batch_size"]
         )
-    
+
     #Define Model
     print("\n########## BUILDING MODEL ##########")
     model = timm.create_model(
@@ -98,10 +107,14 @@ if __name__ == "__main__":
     metrics = get_metrics(cfg["data"]["target_format"])
 
     #Create output folder
-    out_folder = f'{args.output}/{cfg["model"]["name"]}/{cfg["model"]["exp"]}_{datetime.now().strftime("%d-%m-%Y:%H")}'
+    out_folder = f'{args.output}/{cfg["model"]["task"]}-{cfg["model"]["arch"]}-{datetime.now().strftime("%Y-%m-%d-%H:%M")}'
     print(f"Saving weights and logs at '{out_folder}'")
     existsfolder(out_folder)
     existsfolder(out_folder+"/weights")
+
+    #Save copy of config
+    with open(out_folder + "/config.yaml", 'w') as f:
+        yaml.dump(cfg, f, default_flow_style=False)
 
     # Logging
     logger = Logger(cfg, out_folder=out_folder)
@@ -111,13 +124,12 @@ if __name__ == "__main__":
         #Train
         model.train()
         running_loss = 0
-        for i, batch in tqdm.tqdm(enumerate(train_dataloader), unit="Batch", desc="Training", leave=False):
+        for i, batch in tqdm.tqdm(enumerate(train_dataloader), unit="Batch", desc="Training", leave=False, total=len(train_dataloader)):
             #Reset gradients (redundant but sanity check)
             optimizer.zero_grad()
             
             #Seperate batch
             inputs, labels = batch
-            print(inputs, labels)
             
             #Forward
             outputs = model(inputs)
@@ -131,7 +143,7 @@ if __name__ == "__main__":
             optimizer.step()
 
             #logger
-            logger.add_predictions(outputs.to("cpu").numpy(), labels.to("cpu").numpy())
+            logger.add_prediction(outputs.detach().to("cpu").numpy(), labels.detach().to("cpu").numpy())
 
             #Check for loggin frequency
             if i % cfg["wandb"]["log_freq"] == 0:
@@ -143,6 +155,7 @@ if __name__ == "__main__":
                     prepend='train'
                 )
                 running_loss = 0
+                #print(logs)
         
         #Step learning rate
         lr_schedule.step()
@@ -150,10 +163,10 @@ if __name__ == "__main__":
         #Validate
         model.eval()
         running_loss = 0
-        for i, batch in tqdm.tqdm(enumerate(valid_dataloader), unit="Batch", desc="Validating", leave=False):
+        logger.clear_buffer()
+        for i, batch in tqdm.tqdm(enumerate(valid_dataloader), unit="Batch", desc="Validating", leave=False, total=len(valid_dataloader)):
             #Seperate batch
             inputs, labels = batch
-            print(inputs, labels)
             
             #Forward
             outputs = model(inputs)
@@ -163,15 +176,16 @@ if __name__ == "__main__":
             running_loss += loss.item() / len(valid_dataloader)
 
             #Log metrics
-            logger.add_predictions(outputs.to("cpu").numpy(), labels.to("cpu").numpy())
+            logger.add_prediction(outputs.detach().to("cpu").numpy(), labels.detach().to("cpu").numpy())
 
         #Check for loggin frequency
-        logger.clear_buffer()
         logs = logger.log(
             xargs={
-                "loss": running_loss
+                "loss": running_loss / len(valid_dataloader)
             },
             clear_buffer=True,
             prepend='valid'
         )
-        print(logs)
+
+        #Save Model
+        torch.save(model.state_dict(), out_folder + "/weights/" + f'{cfg["model"]["arch"]}-{cfg["model"]["task"]}-Epoch{epoch}.pt')
