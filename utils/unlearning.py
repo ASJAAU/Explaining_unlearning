984eedb9e0e1ac2ad0fcda5b6b0a984e40cb888d
Alex <alex-pv01@proton.me>
2024-06-12 15:38:18 +0200

    confusing kernel

--- utils/unlearning.py
+++ utils/unlearning.py
@@ -21,13 +21,37 @@ def confuse_vision(model, noise_scale, trans = True, reinit_last = True, train_d
     for i, conv in tqdm(enumerate(conv_layers), total=len(conv_layers)):
         print(conv)
         kernel = torch.clone(conv.weight)
+
+        #Confusing kernels
+        for j in range(kernel.shape[0]):
+            for k in range(kernel.shape[1]):
+                print(f"Layer {i}: Kernel ({j},{k})")
+                print(kernel[j,k,:,:])
+                print(kernel[j,k,:,:].shape)
+                #Traspose kernel
+                if trans:
+                    kernel[j,k,:,:] = torch.transpose(kernel[j,k,:,:], 0, 1)
+                print(kernel[j,k,:,:])
+                print(kernel[j,k,:,:].shape)
+                #Compute noise scale proportional to the std of the kernel
+                noise_std = torch.std(kernel[j,k,:,:]).item() * noise_scale
+                print(noise_std)
+                if not noise_std > 0:
+                    print("No noise added")
+                    noise_std = 0.01
+                print("Adding noise")
+                kernel[j,k,:,:] += torch.normal(mean=0, std=noise_std, size=kernel[j,k,:,:].shape)
+                print(kernel[j,k,:,:])
+                print(kernel[j,k,:,:].shape)
+                if j == 0 and k == 0:
+                    break
         
+        #Confusing bias
         if conv.bias:
             bias = torch.clone(conv.bias)
             print(f"Layer {i}: {bias.shape}")
         else: 
             bias = nn.Parameter(torch.zeros(conv.out_channels))
-            #bias = "Anders"
             conv.bias = bias
 
 
@@ -38,12 +62,12 @@ def confuse_vision(model, noise_scale, trans = True, reinit_last = True, train_d
             break
 
     
-    conv_layers = [m for m in model.modules() if isinstance(m, nn.Conv2d)]
-    for i, conv in tqdm(enumerate(conv_layers), total=len(conv_layers)):
-        print(conv.weight)
-        print(conv.bias)
-        if i == 1: 
-            break
+    # conv_layers = [m for m in model.modules() if isinstance(m, nn.Conv2d)]
+    # for i, conv in tqdm(enumerate(conv_layers), total=len(conv_layers)):
+    #     print(conv.weight)
+    #     print(conv.bias)
+    #     if i == 1: 
+    #         break
 
 def forget_loss():
     pass
